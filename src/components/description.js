f6fe9b35121acebe0500bb8efd39c99109abbbf2
Roman Fuentes <romangof@gmail.com>
2017-09-13 11:16:32 -0300

    problem using static.json proxies

--- src/components/description.js
+++ src/components/description.js
@@ -7,7 +7,7 @@ export default class Description extends Component {
   }
 
   componentWillMount() {
-    console.log("fetching");
+    // console.log("fetching");
     // fetch( `/api/catalogo/${this.props.id}`, { // comment this when pushing to heroku
     fetch( '/api/', { // comment this when pushing to heroku
     // fetch(  `https://app-catalogo-web.herokuapp.com/api/catalogo/${this.props.id}`, { // comment this when pushing to heroku
